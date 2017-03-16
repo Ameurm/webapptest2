ee6d926b85738a101e2237107cdf0e19e28e1d21
ameurm <ameur.mezaache@gmail.com>
2017-03-16 19:12:34 -0400

    Add project files.

---
+++ PhoneBookTestApp/PhoneBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace PhoneBookTestApp
+{
+    public class PhoneBook : IPhoneBook
+    {
+
+        public void addPerson(Person newPerson)
+        {
+           
+
+            Person person = new Person();
+            person.name= newPerson.name;
+            person.phoneNumber = newPerson.phoneNumber;
+            person.address = newPerson.address;
+            
+           
+
+       
+
+        }
+
+        public Person findPerson(string firstName, string lastName)
+        {
+           Person person = new Person();
+            var name = from p in person.name
+                       select p;
+            return person;
+            
+        }
+    }
+}
---
+++ PhoneBookTestApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookTestApp
+{
+   public class Program
+    {
+        //private PhoneBook phonebook = new PhoneBook();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+              
+                DatabaseUtil.initializeDatabase();
+                /* TODO: create person objects and put them in the PhoneBook and database
+                * John Smith, (248) 123-4567, 1234 Sand Hill Dr, Royal Oak, MI
+                * Cynthia Smith, (824) 128-8758, 875 Main St, Ann Arbor, MI
+                */
+
+                // TODO: print the phone book out to System.out
+                // TODO: find Cynthia Smith and print out just her entry
+                // TODO: insert the new person objects into the database
+
+                PhoneBook phonebook = new PhoneBook();
+
+                Person person = new Person();
+                person.name = "John Smith";
+                person.phoneNumber = "(248) 123-4567";
+                person.address = "1234 Sand Hill Dr, Royal Oak, MI";
+                string fname = 
+                phonebook.addPerson(person);
+                phonebook.findPerson(person.name.First,person.name.Last);
+                // Console.WriteLine("record added");
+                // Console.ReadLine();
+            }
+            catch (Exception)
+            {
+               throw;
+            }
+            finally
+            {
+                DatabaseUtil.CleanUp();
+            }
+        }
+    }
+}
